3abb5684b656aa3f1149d13b0c79dd20509a7f8a
mads-byte <mbutl224@icloud.com>
2025-10-17 13:05:44 -0400

    simple name and email validation, no styles

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,16 +3,31 @@ import React from "react";
 function Contact() {
 
     const [name, setName] = React.useState("");
+    const [nameError, setNameError] = React.useState("");
+
+    const [email, setEmail] = React.useState("");
+    const [emailError, setEmailError] = React.useState("");
 
     function handleSubmit(e) {
-        e.preventDefault();
-        // submit logic here
+        const nameRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ' -]{2,50}$/
+        const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
+
+        if (!nameRegex.test(name)) {
+            e.preventDefault();
+            setNameError("Please enter a valid name")
+        } else if (!emailRegex.test(name)) {
+            e.preventDefault();
+            setEmailError("Please enter a valid email")
+        } else {
+            setEmailError("")
+            setNameError("")
+        }
     }
 
     return (
         <form className="contact-form" onSubmit={handleSubmit} noValidate>
 
-            <label htmlFor="user-name">Name <span id="name-warning"></span></label>
+            <label htmlFor="user-name">Name <span id="name-warning" >{nameError}</span></label>
             <input
                 id="user-name"
                 name="user-name"
@@ -21,6 +36,15 @@ function Contact() {
                 autoComplete="off"
                 value={name}
                 onChange={(e) => { setName(e.target.value); console.log('name:', e.target.value); }}
+            />
+            <label htmlFor="user-email">Email <span id="email-warning" >{emailError}</span></label>
+            <input
+                id="user-email"
+                name="user-email"
+                type="text"
+                autoComplete="off"
+                value={email}
+                onChange={(e) => { setEmail(e.target.value); console.log('email:', e.target.value); }}
             />
             <button id="submit" type="submit">Send</button>
         </form>
